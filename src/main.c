d3983360832c1169770c2feeb2d19d9d55d32bcc
Ilya Elizarov <ilya.elizarov@tum.de>
2021-05-23 02:01:59 +0200

    Done task 2.1.1 and 2.1.2

--- src/main.c
+++ src/main.c
@@ -22,6 +22,8 @@
 
 #define mainGENERIC_PRIORITY (tskIDLE_PRIORITY)
 #define mainGENERIC_STACK_SIZE ((unsigned short)2560)
+#define degToRad(angleInDegrees) ((angleInDegrees) * M_PI / 180.0)
+#define radToDeg(angleInRadians) ((angleInRadians) * 180.0 / M_PI)
 
 static TaskHandle_t DemoTask = NULL;
 
@@ -42,18 +44,65 @@ void xGetButtonInput(void)
 
 #define KEYCODE(CHAR) SDL_SCANCODE_##CHAR
 
+// Draws a trangle with constant size at given coordinates
+void putTriangleAt(coord_t * points) {
+    // Triangle coordinates
+    const short triangle_half_base = 64;
+    const short triangle_height = 80;
+
+    coord_t coord_triangle[3]= {
+            {points->x - triangle_half_base, points->y + triangle_height/2},
+            {points->x, points->y -triangle_height/2},
+            {points->x + triangle_half_base, points->y + triangle_height/2},
+    };
+
+    tumDrawTriangle(coord_triangle,Black);   
+
+}
+
+// Draws a circle with constant diameter at given coordinates
+void putCircleAt(coord_t * points) {
+    // Circle diameter
+    const short diameter = 80 / 2;
+
+    tumDrawCircle(points->x, points->y, diameter, Red);
+
+}
+
+// Draws a squre with constant side at given coordingates
+void putSquareAt(coord_t * points) {
+    const short width = 80;
+    const short height = width;
+
+    tumDrawFilledBox(points->x-width/2,points->y-height/2,width,height,Blue);
+}
+
 void vDemoTask(void *pvParameters)
 {
-    // structure to store time retrieved from Linux kernel
-    static struct timespec the_time;
-    static char our_time_string[100];
-    static int our_time_strings_width = 0;
-
     // Needed such that Gfx library knows which thread controlls drawing
     // Only one thread can call tumDrawUpdateScreen while and thread can call
     // the drawing functions to draw objects. This is a limitation of the SDL
     // backend.
     tumDrawBindThread();
+
+    // Inital coordinates
+    coord_t triangle_coord= {
+        SCREEN_WIDTH/2,
+        SCREEN_HEIGHT/2
+    };
+    coord_t circle_coord = {
+        SCREEN_WIDTH/2 - 120,
+        SCREEN_HEIGHT/2
+    };
+    coord_t square_coord = {
+        SCREEN_WIDTH/2 + 120,
+        SCREEN_HEIGHT/2
+    };
+
+    // Radians
+    double deg = 0;
+
+    int flag_task21 = 0;
 
     while (1) {
         tumEventFetchEvents(FETCH_EVENT_NONBLOCK); // Query events backend for new events, ie. button presses
@@ -71,16 +120,32 @@ void vDemoTask(void *pvParameters)
             xSemaphoreGive(buttons.lock);
         }
 
+        
+
+
+
+
         tumDrawClear(White); // Clear screen
 
-        clock_gettime(CLOCK_REALTIME,
-                      &the_time); // Get kernel real time
+        circle_coord.x = SCREEN_WIDTH/2  - 120 * cos(degToRad(deg));
+        circle_coord.y = SCREEN_HEIGHT/2 - 120 * sin(degToRad(deg));
+        square_coord.x = SCREEN_WIDTH/2  + 120 * cos(degToRad(deg));
+        square_coord.y = SCREEN_HEIGHT/2  + 120 * sin(degToRad(deg));
 
-        // Format our string into our char array
-        sprintf(our_time_string,
-                "There has been %ld seconds since the Epoch. Press Q to quit",
-                (long int)the_time.tv_sec);
+    
 
+        putTriangleAt(&triangle_coord);
+        
+
+        
+        putCircleAt(&circle_coord);
+        putSquareAt(&square_coord);
+
+        // Increment degrees for clockwise motion of the figures
+        deg++;
+
+
+        /*
         // Get the width of the string on the screen so we can center it
         // Returns 0 if width was successfully obtained
         if (!tumGetTextSize((char *)our_time_string,
@@ -90,11 +155,16 @@ void vDemoTask(void *pvParameters)
                         our_time_strings_width / 2,
                         SCREEN_HEIGHT / 2 - DEFAULT_FONT_SIZE / 2,
                         TUMBlue);
+        */
 
         tumDrawUpdateScreen(); // Refresh the screen to draw string
 
         // Basic sleep of 1000 milliseconds
-        vTaskDelay((TickType_t)1000);
+        if (!flag_task21)
+            vTaskDelay((TickType_t)1000);
+
+        vTaskDelay((TickType_t)50);
+        flag_task21 = 1;
     }
 }
 
