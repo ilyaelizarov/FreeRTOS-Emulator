130b7f66274e1646c219f4ca47b1a6a1e532c677
Ilya Elizarov <ilya.elizarov@tum.de>
2021-05-25 14:32:27 +0200

    Added toggling between the tasks

--- src/main.c
+++ src/main.c
@@ -11,48 +11,56 @@
 #include "semphr.h"
 #include "task.h"
 
-// #include "TUM_Ball.h"
+
 #include "TUM_Draw.h"
 #include "TUM_Utils.h"
 #include "TUM_Event.h"
 #include "TUM_Font.h"
-/*
-#include "TUM_Sound.h"
-#include "TUM_Utils.h"
-#include "TUM_Font.h"
 
-
-#include "AsyncIO.h"
-*/
 #define mainGENERIC_PRIORITY (tskIDLE_PRIORITY)
-#define mainGENERIC_STACK_SIZE ((unsigned short)200)
+#define mainGENERIC_STACK_SIZE ((unsigned short)1000)
 
 static TaskHandle_t BufferSwap = NULL;
-/*static SemaphoreHandle_t ScreenLock = NULL;
-static SemaphoreHandle_t DrawSignal = NULL;
-*/
 
-/* Structure that will hold the TCB of the task being created. */
-StaticTask_t xTaskBuffer;
+// Handles for the tasks
+static TaskHandle_t SwitchingTask1, SwitchingTask2;
 
-/* Buffer that the task being created will use as its stack.  Note this is
-an array of StackType_t variables.  The size of StackType_t is dependent on
-the RTOS port. */
-StackType_t xStack[ mainGENERIC_STACK_SIZE ];
+/* A protected variable for counting button's pushes
+   when we switch between the tasks in exercise 3.2.3 */
+typedef struct switchingButtonCounter {
+    unsigned int counter;
+    SemaphoreHandle_t lock;
+} switchingButtonCounter_t;
 
+switchingButtonCounter_t buttonCounter = { 0 };
+
+// Stores last time when a button was pressed
+TickType_t buttons_last_change;
+
+// Semaphore to resume vSwitchingTask1 in exercise 3.2.3
+SemaphoreHandle_t semSwitchingTask1 = NULL;
+
+// State for the state machine for toggling tasks in exercise 3.2.3
+uint8_t toggle_tasks = 0;
 
 /* configSUPPORT_STATIC_ALLOCATION is set to 1, so the application must provide an
 implementation of vApplicationGetIdleTaskMemory() to provide the memory that is
-used by the Idle task. */
+used by the Idle task.
+This excerpt was taken from https://www.freertos.org/a00110.html without changes */
+
+// Structure that will hold the TCB of the task being created
+StaticTask_t xTaskBuffer;
+// Buffer that the task being created will use as its stack
+StackType_t xStack[ mainGENERIC_STACK_SIZE ];
+
 void vApplicationGetIdleTaskMemory( StaticTask_t **ppxIdleTaskTCBBuffer,
                                     StackType_t **ppxIdleTaskStackBuffer,
-                                    uint32_t *pulIdleTaskStackSize )
-{
-/* If the buffers to be provided to the Idle task are declared inside this
-function then they must be declared static - otherwise they will be allocated on
-the stack and so not exists after this function exits. */
-static StaticTask_t xIdleTaskTCB;
-static StackType_t uxIdleTaskStack[ configMINIMAL_STACK_SIZE ];
+                                    uint32_t *pulIdleTaskStackSize ) {
+    /* If the buffers to be provided to the Idle task are declared inside this
+    function then they must be declared static - otherwise they will be allocated on
+    the stack and so not exists after this function exits. */
+    static StaticTask_t xIdleTaskTCB;
+    static StackType_t uxIdleTaskStack[ configMINIMAL_STACK_SIZE ];
 
     /* Pass out a pointer to the StaticTask_t structure in which the Idle task's
     state will be stored. */
@@ -68,28 +76,6 @@ static StackType_t uxIdleTaskStack[ configMINIMAL_STACK_SIZE ];
 }
 /*-----------------------------------------------------------*/
 
-
-/*
-static TaskHandle_t DemoTask = NULL;
-
-typedef struct buttons_buffer {
-    unsigned char buttons[SDL_NUM_SCANCODES];
-    SemaphoreHandle_t lock;
-} buttons_buffer_t;
-
-static buttons_buffer_t buttons = { 0 };
-
-void xGetButtonInput(void)
-{
-    if (xSemaphoreTake(buttons.lock, 0) == pdTRUE) {
-        xQueueReceive(buttonInputQueue, &buttons.buttons, 0);
-        xSemaphoreGive(buttons.lock);
-    }
-}
-
-#define KEYCODE(CHAR) SDL_SCANCODE_##CHAR
-*/
-
 #define FPS_AVERAGE_COUNT 50
 
 void vDrawFPS(void) {
@@ -99,7 +85,6 @@ void vDrawFPS(void) {
     static unsigned int average_count = 0;
     static TickType_t xLastWakeTime = 0, prevWakeTime = 0;
     static char str[10] = { 0 };
-    static int text_width;
     int fps = 0;
 
     if (average_count < FPS_AVERAGE_COUNT) {
@@ -149,6 +134,81 @@ void putCircleAt(coord_t * points) {
 
 }
 
+
+typedef struct buttons_buffer {
+    unsigned char buttons[SDL_NUM_SCANCODES];
+    SemaphoreHandle_t lock;
+} buttons_buffer_t;
+
+static buttons_buffer_t buttons = { 0 };
+
+void xGetButtonInput(void) {
+    if (xSemaphoreTake(buttons.lock, 0) == pdTRUE) {
+        xQueueReceive(buttonInputQueue, &buttons.buttons, 0);
+        xSemaphoreGive(buttons.lock);
+    }
+}
+
+
+#define KEYCODE(CHAR) SDL_SCANCODE_##CHAR
+#define buttons_DEBOUNCE_DELAY	(200 / portTICK_RATE_MS)
+
+/* Prevents buttons from bouncing:
+   returns 1 if debouncing has been achieved,
+   and 0 if it can't be achieved */
+uint8_t DebouncingCondition (void) {
+
+    const int buttons_change_period = buttons_DEBOUNCE_DELAY;
+
+    if (xTaskGetTickCount() - buttons_last_change > buttons_change_period) {
+        buttons_last_change = xTaskGetTickCount();
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+/* Reacts and shows how many times S have been pushed
+ * for task  3.2.3 */
+void vDrawButtonTextSwitching(void) {
+
+	static char str[100] = { 0 };
+	
+	// Prints the S button counter
+	sprintf(str, "S: %d", buttonCounter.counter);
+	tumDrawText(str, 10, SCREEN_HEIGHT - DEFAULT_FONT_SIZE * 2, Black);
+
+	if (xSemaphoreTake(buttons.lock, 0) == pdTRUE) {
+	
+		// Toggles counting tasks when S is pressed (task 3.2.3)
+		if (buttons.buttons[KEYCODE(S)] && DebouncingCondition()) {
+			
+			toggle_tasks++;
+			
+			if (toggle_tasks == 2)
+				toggle_tasks = 0;
+			
+			switch(toggle_tasks) {
+				// Resume the first task with a semaphore
+				case 0:
+					xSemaphoreGive(semSwitchingTask1);
+					break;
+					
+				// Resume the second task with a 
+				case 1:
+					xTaskNotify(SwitchingTask2, 0, eNoAction);
+					break;
+				
+				default:
+					break;
+			}
+		}
+			
+		xSemaphoreGive(buttons.lock);
+	
+	}
+}
+
 // Blinks with 1 Hz
 void vCircleBlink1(void *pvParameters) {
 
@@ -164,11 +224,11 @@ void vCircleBlink1(void *pvParameters) {
         // Draw a circle in the middle of the screen
         putCircleAt(&circle_coord);
   //      tumDrawUpdateScreen();
-        vTaskDelay((TickType_t)500);
+        vTaskDelay(500 / portTICK_RATE_MS);
         tumDrawClear(White);
    //     tumDrawUpdateScreen();
 
-        vTaskDelay((TickType_t)500);
+        vTaskDelay(500 / portTICK_RATE_MS);
 
   //              tumDrawUpdateScreen();
     }
@@ -187,22 +247,14 @@ void vCircleBlink2(void *pvParameters) {
     while (1) {
         // Draw a circle in the middle of the screen
         putCircleAt(&circle_coord);
- //       tumDrawUpdateScreen();
-
-        vTaskDelay((TickType_t)250);
+        vTaskDelay(250 / portTICK_RATE_MS);
         tumDrawClear(White);
-//        tumDrawUpdateScreen();
-
-        vTaskDelay((TickType_t)250);
-
-     
-
+        vTaskDelay(250 / portTICK_RATE_MS);
     }
 }
 
+void vSwapBuffers(void *pvParameters) {
 
-void vSwapBuffers(void *pvParameters)
-{
     TickType_t xLastWakeTime;
     xLastWakeTime = xTaskGetTickCount();
     const TickType_t frameratePeriod = 20;
@@ -210,129 +262,90 @@ void vSwapBuffers(void *pvParameters)
     tumDrawBindThread(); // Setup Rendering handle with correct GL context
 
     while (1) {
-                        vDrawFPS();
+            tumEventFetchEvents(FETCH_EVENT_NONBLOCK); // Query events backend for new events, ie. button presses
+            xGetButtonInput(); // Update global input
+            vDrawButtonTextSwitching();
+            vDrawFPS();
+
             tumDrawUpdateScreen();
-  /*          tumEventFetchEvents(FETCH_EVENT_BLOCK);
-            xSemaphoreGive(ScreenLock);
-            xSemaphoreGive(DrawSignal);*/
-            vTaskDelayUntil(&xLastWakeTime,
-            frameratePeriod);
-  //                          pdMS_TO_TICKS(frameratePeriod));
-        //}
+            vTaskDelayUntil(&xLastWakeTime, frameratePeriod);
     }
 }
 
+// The task that requires a binary semaphore
+void vSwitchingTask1 (void *pvParameters) {
 
+	while(1) {
+	
+		// Blocks to wait for a semaphore
+		if (xSemaphoreTake(semSwitchingTask1, portMAX_DELAY) == pdPASS) {
 
-/*
-void vDemoTask(void *pvParameters)
-{
-    // structure to store time retrieved from Linux kernel
-    static struct timespec the_time;
-    static char our_time_string[100];
-    static int our_time_strings_width = 0;
-
-    // Needed such that Gfx library knows which thread controlls drawing
-    // Only one thread can call tumDrawUpdateScreen while and thread can call
-    // the drawing functions to draw objects. This is a limitation of the SDL
-    // backend.
-    tumDrawBindThread();
-
-    while (1) {
-        tumEventFetchEvents(FETCH_EVENT_NONBLOCK); // Query events backend for new events, ie. button presses
-        xGetButtonInput(); // Update global input
-
-        // `buttons` is a global shared variable and as such needs to be
-        // guarded with a mutex, mutex must be obtained before accessing the
-        // resource and given back when you're finished. If the mutex is not
-        // given back then no other task can access the reseource.
-        if (xSemaphoreTake(buttons.lock, 0) == pdTRUE) {
-            if (buttons.buttons[KEYCODE(
-                                    Q)]) { // Equiv to SDL_SCANCODE_Q
-                exit(EXIT_SUCCESS);
-            }
-            xSemaphoreGive(buttons.lock);
-        }
-
-        tumDrawClear(White); // Clear screen
-
-        clock_gettime(CLOCK_REALTIME,
-                      &the_time); // Get kernel real time
-
-        // Format our string into our char array
-        sprintf(our_time_string,
-                "There has been %ld seconds since the Epoch. Press Q to quit",
-                (long int)the_time.tv_sec);
-
-        // Get the width of the string on the screen so we can center it
-        // Returns 0 if width was successfully obtained
-        if (!tumGetTextSize((char *)our_time_string,
-                            &our_time_strings_width, NULL))
-            tumDrawText(our_time_string,
-                        SCREEN_WIDTH / 2 -
-                        our_time_strings_width / 2,
-                        SCREEN_HEIGHT / 2 - DEFAULT_FONT_SIZE / 2,
-                        TUMBlue);
-
-        tumDrawUpdateScreen(); // Refresh the screen to draw string
-
-        // Basic sleep of 1000 milliseconds
-        vTaskDelay((TickType_t)1000);
-    }
+			if (xSemaphoreTake(buttonCounter.lock, 0) == pdPASS) {
+				buttonCounter.counter++;
+				xSemaphoreGive(buttonCounter.lock);
+			}
+		}
+	}
 }
-*/
+
+// The task that requires a notification
+void vSwitchingTask2 (void *pvParameters) {
+
+	while(1) {
+	
+		// Blocks to wait for a notification
+		ulTaskNotifyTake( pdTRUE, portMAX_DELAY );
+
+		if (xSemaphoreTake(buttonCounter.lock, 0) == pdPASS) {
+			buttonCounter.counter++;
+			xSemaphoreGive(buttonCounter.lock);
+		}
+	}
+}
+ 
+// The task that resets the button's counter
+void vSwitchingTimer (void *pvParameters) {
+
+    TickType_t xLastWakeTimeTimer;
+
+    xLastWakeTimeTimer = xTaskGetTickCount();
+
+	while(1) {
+		if (xSemaphoreTake(buttonCounter.lock, 0) == pdPASS) {
+			buttonCounter.counter = 0;	
+			xSemaphoreGive(buttonCounter.lock);
+		}
+
+        vTaskDelayUntil(&xLastWakeTimeTimer, 10000 / portTICK_RATE_MS);
+
+	}
+    
+
+}
 
 int main(int argc, char *argv[]) {
 
     char *bin_folder_path = tumUtilGetBinFolderPath(argv[0]);
 
-    printf("Initializing: ");
 
-    if (tumDrawInit(bin_folder_path)) {
-        PRINT_ERROR("Failed to initialize drawing");
-        goto err_init_drawing;
-    }
+    tumDrawInit(bin_folder_path); // Inialize drawing
+    tumEventInit(); // Initalize events
 
-/*
-    if (tumEventInit()) {
-        PRINT_ERROR("Failed to initialize events");
-        goto err_init_events;
-    }
-
-    if (tumSoundInit(bin_folder_path)) {
-        PRINT_ERROR("Failed to initialize audio");
-        goto err_init_audio;
-    }
-
-    buttons.lock = xSemaphoreCreateMutex(); // Locking mechanism
-    if (!buttons.lock) {
-        PRINT_ERROR("Failed to create buttons lock");
-        goto err_buttons_lock;
-    }
-*/
+    semSwitchingTask1 = xSemaphoreCreateBinary(); // Locking mechanism for the first task in the exercise 3.2.3
+    buttonCounter.lock = xSemaphoreCreateMutex(); // Mutex to protect buttonCounter.counter
+    buttons.lock = xSemaphoreCreateMutex(); // Locking mechanism for buttons
 
     xTaskCreate(vSwapBuffers, "BufferSwapTask", mainGENERIC_STACK_SIZE, NULL, configMAX_PRIORITIES, BufferSwap);
-
     xTaskCreate(vCircleBlink1, "Blinks1Hz", mainGENERIC_STACK_SIZE, NULL, mainGENERIC_PRIORITY + 1, NULL);
-
     xTaskCreateStatic(vCircleBlink2, "Blinks2Hz", mainGENERIC_STACK_SIZE, NULL, mainGENERIC_PRIORITY , xStack, &xTaskBuffer);
-   
+    xTaskCreate(vSwitchingTask1, "SwitchingTask1", mainGENERIC_STACK_SIZE, NULL, tskIDLE_PRIORITY, &SwitchingTask1);
+    xTaskCreate(vSwitchingTask2, "SwitchingTask2", mainGENERIC_STACK_SIZE, NULL, tskIDLE_PRIORITY, &SwitchingTask2);
+    xTaskCreate(vSwitchingTimer, "SwitchingTimer", mainGENERIC_STACK_SIZE, NULL, tskIDLE_PRIORITY + 1, NULL);
+
     vTaskStartScheduler();
 
     return EXIT_SUCCESS;
 
-/*
-err_demotask:
-    vSemaphoreDelete(buttons.lock);
-err_buttons_lock:
-    tumSoundExit();
-err_init_audio:
-    tumEventExit();
-err_init_events:
-    tumDrawExit();
-*/
-err_init_drawing:
-    return EXIT_FAILURE;
 }
 
 // cppcheck-suppress unusedFunction
